cc6790581a263b3d50f39da925100f4bb215e30f
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github.com>
2023-12-10 03:13:51 -0300

    graphykit coming soon...

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1>Graphykit</h1>
+          <h5>coming soon...</h5>
+        </div>
+
+        <div className='Link-wrapper'>
+          <p>
+            by{'  '}<code><a
+              className="App-link"
+              href="https://numax.herokuapp.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              NumaX
+            </a></code>
+          </p>
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
